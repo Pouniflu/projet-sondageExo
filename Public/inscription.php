c3dee04884b783fdb1e2e2a31dfdcf67041bb802
Nicolas de Garrigues <ndegarrigues@gmail.com>
2020-11-19 11:41:10 +0100

    connect bdd to inscription

--- Public/inscription.php
+++ Public/inscription.php
@@ -1,3 +1,70 @@
+<?php
+
+use Core\Database;
+
+    if(isset($_POST['formInscription']))
+    {
+
+        $firstName = htmlspecialchars($_POST['firstName']);
+        $lastName = htmlspecialchars($_POST['lastName']);
+        $pseudo = htmlspecialchars($_POST['pseudo']);
+        $email = htmlspecialchars($_POST['email']);
+        $email2 = htmlspecialchars($_POST['email2']);
+        $mdp = sha1($_POST['mdp']);
+        $mdp2 = sha1($_POST['mdp2']);
+
+
+        if(!empty($_POST['lastName']) AND !empty($_POST['firstName']) AND !empty($_POST['pseudo']) AND !empty($_POST['email']) AND !empty($_POST['email2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) {
+
+            $lastNamelength = strlen($lastName);
+            $firstNamelength = strlen($firstName);
+            $pseudolength = strlen($pseudo);
+            
+            if($lastNamelength <= 255){
+                if($firstNamelength <= 255) {
+                    if($pseudolength <= 255){
+                
+                        if($email == $email2) {
+
+                            if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                                if($mdp == $mdp2) {
+
+                                    // $insertmbr = $pdo->prepare("INSERT INTO t_utilisateurs(lastName, firstName, pseudo, email, mdp) VALUES(?, ?, ?, ?, ?)");
+                                    // $insertmbr->execute(array($lastName, $firstName, $pseudo, $email, $mdp));
+                                    // $erreur = "Votre compte a bien été créé";
+
+                                } else {
+                                    $erreur = "Vos mdp sont différents";
+                                }
+                            } else {
+                                $erreur = "Ton adresse mail n'est pas valide !";
+                            }
+
+                        } else {
+                            $erreur = "Vos adresses emails sont différentes";
+                        }
+
+                    } else {
+                        $erreur = "Votre pseudo ne doit pas dépasser 255 caractères";
+                    }
+                } else {
+                    $erreur = "Votre Prénom ne doit pas dépasser 255 caractères";
+                }
+            } else {
+                $erreur = "Votre Nom ne doit pas dépasser 255 caractères";
+            }
+
+
+
+        } else {
+            $erreur = "*Tous les champs doivent être complétés";
+        }
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +85,7 @@
                         <label for="lastName">Nom :</label>
                     </td>
                     <td>
-                        <input type="text" placeholder="Votre nom" id="lastName" name="lastName">
+                        <input type="text" placeholder="Votre nom" id="lastName" name="lastName" value="<?php if(isset($lastName)) { echo $lastName; } ?>">
                     </td>
                 </tr>
 
@@ -27,7 +94,7 @@
                         <label for="firstName">Prénom :</label>
                     </td>
                     <td>
-                        <input type="text" placeholder="Votre prénom" id="firstName" name="firstName">
+                        <input type="text" placeholder="Votre prénom" id="firstName" name="firstName" value="<?php if(isset($firstName)) { echo $firstName; } ?>">
                     </td>
                 </tr>
 
@@ -36,7 +103,7 @@
                         <label for="pseudo">Pseudo :</label>
                     </td>
                     <td>
-                        <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo">
+                        <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>">
                     </td>
                 </tr>
 
@@ -45,7 +112,7 @@
                         <label for="email">Email :</label>
                     </td>
                     <td>
-                        <input type="email" placeholder="Votre email" id="email" name="email">
+                        <input type="email" placeholder="Votre email" id="email" name="email" value="<?php if(isset($email)) { echo $email; } ?>">
                     </td>
                 </tr>
 
@@ -54,7 +121,7 @@
                         <label for="email2">Confirmation Email :</label>
                     </td>
                     <td>
-                        <input type="email" placeholder="Confirmer votre email" id="email2" name="email2">
+                        <input type="email" placeholder="Confirmer votre email" id="email2" name="email2" value="<?php if(isset($email2)) { echo $email2; } ?>">
                     </td>
                 </tr>
 
@@ -76,11 +143,24 @@
                     </td>
                 </tr>
 
-            </table>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="submit" name="formInscription" value="Je m'inscris !">
+                    </td>
+                </tr>
 
-            <input type="submit" value="Je m'inscris !">
-        
+            </table>
         </form>
+
+        <?php
+
+            if(isset($erreur)) {
+                echo '<font color="red">' . $erreur . "</font>";
+            }
+
+        ?>
+
     </div>
     
 </body>
