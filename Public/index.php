c653fa1337af317124ec94c54c0f7841e1cdc236
Claire <claire.brisbart@edu.devinci.fr>
2020-11-20 16:57:03 +0100

    :poop: Add autoloader

--- Public/index.php
+++ Public/index.php
@@ -2,8 +2,7 @@
 use App\Controller\HomeController;
 
 define("ROOT", dirname(__DIR__));
-require ROOT."/Autoloader.php";
-Autoloader::register();
+require ROOT."\\vendor\\autoload.php";
 
 // Instanciation de la classe HomeController
 // On lui applique ensuite la méthode renderIndex() qui provient du controller : ça permet de faire le lien avec la view
