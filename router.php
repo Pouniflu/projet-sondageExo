6174069777be278c65d9525c98a47e121c85cb3c
Claire <claire.brisbart@edu.devinci.fr>
2020-11-22 18:44:44 +0100

    :memo: Add router explication :bug: fix a bug

--- router.php
+++ router.php
@@ -1,4 +1,10 @@
 <?php
+// UTILISATION DU ROUTER :
+// 1. Ajouter la classe utilisée en premier avec "use"
+// 2. Créer un nouveau "case"
+// exemple : si on va sur l'url projet-sondageexo.test/Public/?page=result on se retrouve sur la View Result parce qu'on a
+// appelé la méthode renderIndex() (cf App\Controller\ResultPollController pour voir cette méthode).
+
 
 use App\Controller\HomeController;
 use App\Controller\ResultPollController;
@@ -13,10 +19,11 @@ if(array_key_exists("page", $_GET)){
         case 'results':
             $controller = new ResultPollController();
             $controller->renderIndex();
+            break;
         case 'createPoll':
             $controller = new CreatePollController();
             $controller->renderIndex();
-
+            break;
         default:
             # code...
             break;
